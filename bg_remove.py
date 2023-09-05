385f0959886fde89e8ed257630a7a145a3d2b59d
shubham ralli <sralli73@gmail.com>
2023-09-05 18:52:45 +0530

    newy

--- bg_remove.py
+++ bg_remove.py
@@ -8,6 +8,7 @@ from io import BytesIO
 
 bg_remove_bp = Blueprint('bg_remove', __name__)
 
+# Route to upload and convert CSV to JSON
 @bg_remove_bp.route('/remove_bg', methods=['POST'])
 def api_remove_background():
     try:
@@ -19,10 +20,15 @@ def api_remove_background():
         if input_image.filename == '':
             return 'No selected image file', 400
 
-        # Perform background removal
+        # Check if the file type is allowed (e.g., JPEG, PNG)
+        allowed_extensions = {'jpg', 'jpeg', 'png'}
+        if '.' not in input_image.filename or input_image.filename.split('.')[-1].lower() not in allowed_extensions:
+            return 'Invalid file type. Only JPEG and PNG are supported.', 400
+
+        # Process the image
         output_image = remove_background(input_image)
 
-        # Convert the output image to base64
+        # Convert the processed image to base64
         buffered = BytesIO()
         output_image.save(buffered, format="PNG")
         result_image_base64 = base64.b64encode(buffered.getvalue()).decode()
@@ -38,9 +44,18 @@ def api_remove_background():
         return jsonify(response_data)
 
     except Exception as e:
-        return str(e), 400
-
+        # Log the exception for debugging
+        print(str(e))
+        return 'An error occurred while processing the image', 500
+    
+# Function to remove background from an image
 def remove_background(input_image):
     with Image.open(input_image) as img:
         output = remove(img)
         return output
+
+if __name__ == '__main__':
+    app = Flask(__name__)
+    CORS(app)
+    app.register_blueprint(bg_remove_bp)
+    app.run(host='0.0.0.0', port=5000)  # Use a production-ready server like Gunicorn in a production environment.
